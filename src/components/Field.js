ca0a45a9dc8b6b3170fffba645fc69607dbf0afa
bradleysimmons <bradleysimmons9@gmail.com>
2019-06-22 11:42:30 -0500

    field component

---
+++ src/components/Field.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Field extends ReactComponent {
+  render () {
+    return (
+      <div className="ui field">
+        <label>name</label>
+        <input />
+
+      </div>
+    )
+  }
+}
+
+export default Field;
