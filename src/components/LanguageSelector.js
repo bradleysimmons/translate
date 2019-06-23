229ed2390f1ea659b9dcc3db0a9184d33896dc50
bradleysimmons <bradleysimmons9@gmail.com>
2019-06-23 15:25:32 -0500

    updates button and field

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class LanguageSelector extends React.Component {
+
+  static contextType = LanguageContext;
+
   render() {
+    console.log(this.context)
     return(
       <div>
         select a language:
-        <i className='flag us' onClick={ () => this.props.onLanguageChange('english')}/>
-        <i className='flag mx' onClick={ () => this.props.onLanguageChange('spanish')}/>
+        <i className='flag us' onClick={ () => this.context.onLanguageChange('english')}/>
+        <i className='flag mx' onClick={ () => this.context.onLanguageChange('spanish')}/>
       </div>
     )
   }
