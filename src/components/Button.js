8f9230eed51d081f82f0dbdac1413e2f632795fe
bradleysimmons <bradleysimmons9@gmail.com>
2019-06-22 23:18:29 -0500

    wraps in a provider

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
-    return <button className="ui button primary">submit</button>;
+    const text = this.context === 'english' ? 'submit' : 'enviar'
+    return <button className="ui button primary">{text}</button>;
+    
   }
 }
 
